ab527b1a4503eb3ffc267bf42fe6d0c7d6bb48ba
shafinul98 <shafinul@devolvedai.com>
2023-09-17 08:53:44 +0600

    Removed MetaData Tag for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import Header from "@/components/Custom Components/Header";
 import "./globals.css";
-import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import Footer from "@/components/Custom Components/Footer";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,11 +11,6 @@ const lato = Lato({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
 });
-
-export const metadata: Metadata = {
-  title: "Devolved AI",
-  description: "Devolved AI Home Page",
-};
 
 export default function RootLayout({
   children,
