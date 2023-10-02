ced10430a36d9d1739a76c011ff0a562fecae8e2
shafinul98 <shafinul@devolvedai.com>
2023-10-02 12:58:19 +0600

    Fixed mobile menu issues

--- components/Custom Components/Header.tsx
+++ components/Custom Components/Header.tsx
@@ -2,12 +2,7 @@
 
 import MenuIcon from "../../public/Menu Icon.svg";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { AiOutlineClose } from "react-icons/ai";
 
 import Image from "next/image";
 import React, { useState } from "react";
@@ -41,46 +36,34 @@ const Header = () => {
               <Image src={MenuIcon} alt="Menu Icon" />
             </div>
 
-            <div className={!isMenuOpen ? "hidden" : "flex"}>
+            <div className={!isMenuOpen ? "hidden" : "flex w-full"}>
               <div
-                className="w-10 h-10 flex border items-center justify-center rounded-sm p-1 me-3.5"
-                onClick={() => {
-                  setIsMenuOpen(!isMenuOpen);
-                }}
+                className={
+                  isMenuOpen
+                    ? "fixed z-50 right-0 top-0 w-[60%] h-full border-r border-r-gray-400 bg-[#FFF] transition ease-in duration-1000"
+                    : "fixed transition right-[-100%] ease-out duration-1000"
+                }
               >
-                <Image src={MenuIcon} alt="Menu Icon" />
-              </div>
-              <nav
-                id="sidenav-2"
-                className="fixed right-0 top-0 z-[1035] h-screen w-80 transition duration-500 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
-                data-te-sidenav-init
-                data-te-sidenav-hidden="false"
-                data-te-sidenav-mode="side"
-                data-te-sidenav-content="#content"
-              >
-                <div className="flex sm:max-md:my-5 md:my-[1.25rem] items-center justify-center">
-                  <Link href={"/"} className="me-10">
+                <div className="flex items-center md:justify-between justify-center pt-1 px-2">
+                  <Link href={"/"} className="xl:pr-28 2xl:me-auto 2xl:pl-12">
                     <Image
                       src={Logo}
                       alt="Devolved AI Logo"
                       quality={100}
-                      className="w-3/4"
+                      className="w-full"
                     />
                   </Link>
                   <div
-                    className="w-10 h-10 flex border items-center justify-center rounded-sm p-1 me-3.5"
+                    className="w-10 h-10 flex items-center justify-center rounded-sm p-1"
                     onClick={() => {
                       setIsMenuOpen(!isMenuOpen);
                     }}
                   >
-                    <Image src={MenuIcon} alt="Menu Icon" />
+                    <AiOutlineClose />
                   </div>
                 </div>
 
-                <ul
-                  className="relative m-0 list-none px-[1.25rem] py-[1.25rem] flex flex-col gap-5"
-                  data-te-sidenav-menu-ref
-                >
+                <ul className="uppercase pt-5 min-h-screen flex flex-col gap-5 items-start pl-5">
                   <li
                     className="relative"
                     onClick={() => {
@@ -152,9 +135,10 @@ const Header = () => {
                     </Link>
                   </li>
                 </ul>
-              </nav>
+              </div>
             </div>
           </div>
+
           {/* Mobile and Above NavBar */}
           <div className="p-3 xl:ms-0 lg:justify-center xl:justify-between items-center xl:gap-10 lg:gap-5 hidden lg:flex text-sm text-[#2D3748]">
             <Link className="text-[1rem]" href="/">
