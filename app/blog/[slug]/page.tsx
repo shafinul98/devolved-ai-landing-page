d02ea8e182f9e5e04748edcd702cb7c80af0c204
shafinul98 <shafinul@devolvedai.com>
2023-10-04 13:02:16 +0600

    Removed cross button Menu closes on outside click Blogs have been integrated

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+"use client";
 
-const BlogPage = () => {
-  return <div>BlogPage</div>;
+import { Metadata, ResolvingMetadata } from "next";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const BlogPage = ({ params }: { params: { slug: string } }) => {
+  type Blog = {
+    _id: string;
+    title: string;
+    slug: string;
+    description: string;
+    image: string;
+  };
+
+  const [blog, setBlog] = useState<Blog>({
+    _id: "",
+    title: "",
+    slug: "",
+    description: "",
+    image: "",
+  });
+
+  const fetchBlog = async () => {
+    const response = await fetch(`/api/blogs/${params.slug}`, {
+      method: "GET",
+    });
+
+    return await response.json();
+  };
+
+  useEffect(() => {
+    fetchBlog().then(({ data }) => {
+      setBlog(data);
+    });
+  }, []);
+
+  return (
+    <main className="flex min-h-screen overflow-x-hidden flex-col items-start justify-between">
+      {/* Blog Header */}
+      <section className="flex flex-col mt-10 min-h-screen 3xl:container 2xl:max-3xl:px-28 xl:max-2xl:px-24 lg:max-xl:px-14 w-full">
+        <h1 className="xl:text-[2rem] lg:text-[1.5rem] text-[#2D3748] font-extrabold">
+          {blog.title}
+        </h1>
+        {/* Blog Image */}
+        <Image
+          src={blog.image}
+          alt={blog.title}
+          width={500}
+          height={500}
+          className="my-5"
+          priority={true}
+          style={{ width: "100%", objectFit: "cover", height: "30%" }}
+        />
+        {/* Blog Content */}
+        <article className="mt-10">
+          <p className="text-[#646E73] text-[1.25rem] text-justify leading-relaxed whitespace-break-spaces">
+            {blog.description}
+          </p>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default BlogPage;
--- components/Custom Components/Header.tsx
+++ components/Custom Components/Header.tsx
@@ -5,7 +5,7 @@ import MenuIcon from "../../public/Menu Icon.svg";
 import { AiOutlineClose } from "react-icons/ai";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Logo from "../../public/Devolved AI Logo.svg";
 import Link from "next/link";
 import Button from "./Button";
@@ -13,6 +13,27 @@ import { EarlyAccessForm } from "./EarlyAccessForm";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Function to handle clicks outside the menu
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    // Attach the event listener when the menu is open
+    if (isMenuOpen) {
+      window.addEventListener("click", handleClickOutside);
+    }
+
+    // Clean up the event listener when the component unmounts or the menu closes
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [isMenuOpen]);
 
   return (
     <header>
@@ -26,7 +47,7 @@ const Header = () => {
               className="w-full"
             />
           </Link>
-          <div className="lg:hidden">
+          <div className="lg:hidden" ref={menuRef}>
             <div
               className="w-10 h-10 flex border items-center justify-center rounded-sm p-1 me-3.5"
               onClick={() => {
@@ -43,15 +64,8 @@ const Header = () => {
                   : "fixed z-50 right-[-100%] ease-in duration-500"
               }
             >
-              <div className="flex items-center md:justify-between justify-end pt-1 px-2">
-                <div
-                  className="w-10 h-10 flex items-center justify-end rounded-sm p-1"
-                  onClick={() => {
-                    setIsMenuOpen(!isMenuOpen);
-                  }}
-                >
-                  <AiOutlineClose />
-                </div>
+              <div className="flex items-center md:justify-between justify-start pt-1 px-2">
+                <div className="w-10 h-10 flex items-center justify-end rounded-sm p-1"></div>
               </div>
 
               <ul className="uppercase pt-5 min-h-screen flex flex-col gap-5 items-start pl-5">
