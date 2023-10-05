e8ca0f3672704e254642217bddbb0e51ea38de0a
shafinul98 <shafinul@devolvedai.com>
2023-10-05 09:33:03 +0600

    Made some changes to the gap in title on home

--- app/page.tsx
+++ app/page.tsx
@@ -44,11 +44,11 @@ export default function Home() {
       <section className="w-full md:bg-[#f5f5f5]" id="curved-rectangle">
         <div className="flex flex-col items-center md:flex-row md:justify-center md:p-12 md:pl-12 lg:px-16 lg:pl-[4rem] xl:px-28 xl:pl-32 2xl:pl-[7.5rem] 3xl:container md:mt-[2.1rem]">
           <div className="flex flex-col md:gap-5 mb-10 md:me-[6rem] lg:me-[10.5rem] xl:me-[7.5rem]">
-            <h1 className="font-bold text-[2rem] text-start md:text-[3rem] md:leading-[1.15] text-[#2D3748]">
+            <h1 className="font-bold text-[2rem] text-start md:text-[3rem] md:leading-[1.15] mb-3 md:mb-0 text-[#2D3748]">
               Empowering <span className="text-[#0074D9]">You</span> to Shape
               the Future of Artificial Intelligence
             </h1>
-            <p className="font-normal text-[1rem] md:text-start md:text-xl text-[#646E73]">
+            <p className="font-normal text-[1rem] mb-6 md:mb-0 md:text-start md:text-xl text-[#646E73]">
               AI is transforming our world, and its impact will touch every life
               on Earth. This is your chance to not just witness, but to actively
               shape that transformation.
