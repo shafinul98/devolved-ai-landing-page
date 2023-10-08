fe45ac4d6cea30c47ae5d24455aa49082ebac8fe
shafinul98 <shafinul@devolvedai.com>
2023-10-08 15:25:16 +0600

    Added a comment on email octopus route

--- app/api/emailOctopus/route.ts
+++ app/api/emailOctopus/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// This function handles the logic of making a request to EmailOctopus to sign up a user for our newsletter
 export async function POST(req: NextRequest, res: NextResponse) {
   const data = await req.json();
 
