0bbb7ebbb7459ea3c3ea53785db293894981b474
shafinul98 <shafinul@devolvedai.com>
2023-11-19 13:10:37 +0600

    Minor changes added into .d.ts file

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,7 +4,6 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
 declare namespace JSX {
   interface IntrinsicElements {
     "form-widget": any;
