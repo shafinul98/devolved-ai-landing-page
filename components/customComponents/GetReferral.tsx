bbee5db5109991d5ffdfe3c6b8acfa22ad5c4112
shafinul98 <shafinul@devolvedai.com>
2023-11-20 14:32:19 +0600

    Loading text added into the campaign

--- components/customComponents/GetReferral.tsx
+++ components/customComponents/GetReferral.tsx
@@ -1,10 +1,26 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function GetReferral() {
-    const UCID = "EeQTn0VkqudBHfkE9leVA0AcX70";
-    return (
-        <div className="mt-5 mb-5">
-            <form-widget ucid={UCID}></form-widget>
-        </div>
-    );
+  const UCID = "EeQTn0VkqudBHfkE9leVA0AcX70";
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://app.viral-loops.com/widgetsV2/core/loader.js";
+    script.async = true;
+    script.onload = () => setLoading(false);
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div className="mt-5 mb-5">
+      {loading ? (
+        <div>Loading Campaign...</div>
+      ) : (
+        <form-widget ucid={UCID}></form-widget>
+      )}
+    </div>
+  );
 }
