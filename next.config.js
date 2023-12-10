db02e4b848983cda8383128ffeb4aa9274ddb76a
tanvir-devolvedai <tanvir@devolvedai.com>
2023-12-10 10:57:16 +0600

    restore config files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
 };
 
 module.exports = nextConfig;
