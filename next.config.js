f991350a6615ca626842f411b8200408f1c84b19
tanvir-devolvedai <tanvir@devolvedai.com>
2023-12-10 10:55:47 +0600

    added new page notarized-identity-statement.pdf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
