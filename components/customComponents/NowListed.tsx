3ab22c2c1bca61f0a8bb8a421981d14f1c620e42
Tanvir Aunjum <150123014+tanvir-devolvedai@users.noreply.github.com>
2023-12-24 09:53:08 +0600

    Update NowListed.tsx
    changed text size in mobile

--- components/customComponents/NowListed.tsx
+++ components/customComponents/NowListed.tsx
@@ -9,11 +9,11 @@ export default function NowListed () {
     return (
         <Link href={"https://app.uniswap.org/swap?chain=polygon"} target='_blank'
         className="fixed w-full xl:h-[2rem] bg-[#FF6F00] top-16 xl:top-28 flex items-center justify-center p-2 xl:z-50 z-10 m-5 xl:m-0">
-            <span className="text-[#ffffff] text-[1rem] xl:text-sm uppercase leading-normal">
+            <span className="text-[#ffffff] text-[0.70rem] xl:text-sm uppercase leading-normal">
                 Argocoin now listed on Uniswap. Trade now!
             </span>
             <span>
-                <Image src={ArrowUpRight} alt="ArrowUpRight2" className='ml-3' />
+                <Image src={ArrowUpRight} alt="ArrowUpRight2" className='xl:ml-3 ml-2' />
             </span>
         </Link>
     );
