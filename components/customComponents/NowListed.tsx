de432db7a0cf9776960b053a6a341829ee7a8215
Tanvir Aunjum <150123014+tanvir-devolvedai@users.noreply.github.com>
2023-12-24 09:44:11 +0600

    Update NowListed.tsx
    changed sticky bar size

--- components/customComponents/NowListed.tsx
+++ components/customComponents/NowListed.tsx
@@ -8,8 +8,8 @@ import Link from 'next/link'
 export default function NowListed () {
     return (
         <Link href={"https://app.uniswap.org/swap?chain=polygon"} target='_blank'
-        className="fixed w-full bg-[#FF6F00] top-16 xl:top-28 flex items-center justify-center p-4 xl:z-50 z-10 m-5 xl:m-0">
-            <span className="text-[#ffffff] text-[1rem] xl:text-[1.5rem] uppercase leading-normal">
+        className="fixed w-full xl:h-[2rem] bg-[#FF6F00] top-16 xl:top-28 flex items-center justify-center p-2 xl:z-50 z-10 m-5 xl:m-0">
+            <span className="text-[#ffffff] text-[1rem] xl:text-sm uppercase leading-normal">
                 Argocoin now listed on Uniswap. Trade now!
             </span>
             <span>
